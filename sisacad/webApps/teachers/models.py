8519bc350a8806fb083388c9408db570134174cb
JBenjamin01 <jmamanices@unsa.edu.pe>
2024-06-10 08:26:03 -0500

    Está un poquito complicado cambiar el tipo de ID con el objeto que ya se ingresó.

--- sisacad/webApps/teachers/models.py
+++ sisacad/webApps/teachers/models.py
@@ -3,7 +3,8 @@ import uuid
 
 # Create your models here.
 class Teacher(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    """id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)"""
+    id = models.BigAutoField(primary_key=True)
     email = models.EmailField()
     names = models.CharField(max_length=255)
     lastnames = models.CharField(max_length=255)
